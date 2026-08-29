d68c5ca46b114c1796118ebc4b02edf840fc6d33
Chris-Desktop <Chris@desktop.com>
2026-08-29 14:56:49 -0700

    Added a docker container based server.

---
+++ app/backend/server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import Blueprint, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from Docker!\n'
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
